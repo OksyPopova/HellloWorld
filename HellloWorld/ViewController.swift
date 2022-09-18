416250f999804b1459966ea6dc9835100c1a1287
OksyPopova <oxana12345616@gmail.com>
2022-09-18 15:32:06 +0300

    Custom system button

--- HellloWorld/ViewController.swift
+++ HellloWorld/ViewController.swift
@@ -16,15 +16,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         greetingLable.isHidden.toggle()
         greetingButton.layer.cornerRadius = 10
+        greetingButton.configuration = setupButton(with: "Show greeting")
     }
     
     @IBAction func greetingButtonDidTaaped() {
         greetingLable.isHidden.toggle()
-        greetingButton.setTitle(
-            greetingLable.isHidden ? "Show greeting" : "Hide greeting",
-            for: .normal
+        greetingButton.configuration = setupButton(
+            with: greetingLable.isHidden ? "Show Greeting" : "Hide Greeting"
         )
         
     }
-    
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.title = title
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
+        buttonConfiguration.cornerStyle = .large
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
+        return buttonConfiguration
+    }
 }
